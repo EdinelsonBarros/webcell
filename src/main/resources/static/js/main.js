f2ddcd6f2b6ed2187d783ad7ae9e376c8ae92c0f
Edinelson Lima Barros <05741124135@tocantins.governo>
2025-06-25 15:09:28 -0300

    view finalizada, validação de data incompleta

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,8 +23,8 @@ function restoreTableFromSession() {
 					td.text(rowData[colName]);
 				}
 			});
-			
-			if(rowData.id){
+
+			if (rowData.id) {
 				tr.attr('data-id', rowData.id);
 			}
 		}
@@ -42,33 +42,41 @@ function handleCellEdit() {
 	let rowIndex = tr.index();
 	let colName = td.data('col');
 	let value = td.text().trim();
-	
-	if(value == '') return;
+
+	if (value == '') return;
 
 	const rowData = JSON.parse(sessionStorage.getItem('linha_' + rowIndex)) || {};
-		
-	if(rowData.id){
-		saveToSession(rowIndex, colName, value)
-		sendPatch(rowIndex, td, colName);
-		return
-	}		
 
-		if (rowData[colName] === value) { // nada mudou, não precisa salvar de novo
-			return;
-		} else {
-			if (isDateField(colName)) {
-				// Evita criar múltiplos inputs se já estiver editando
-				if (!td.find('input').length) {
-					createDateInput(td, value, rowIndex, colName, tr);
-				}
-				return; // Evita salvar o valor antes da validação
+	if (rowData[colName] === value) return;  // nada mudou, não precisa salvar de novo
+
+	// Ser tiver id 
+	if (rowData.id) {
+		if (isDateField(colName)) {
+			// Evita criar múltiplos inputs se já estiver editando
+			if (!td.find('input').length) {
+				createDateInput(td, value, rowIndex, colName, tr);
 			}
-			// Para outros campos, salva normalmente
-			saveToSession(rowIndex, colName, value);
-			checkAndSubmitRow(tr, rowIndex, colName);
+			return; // Evita salvar o valor antes da validação
 		}
-
+		// Para outros campos, salva normalmente
+		saveToSession(rowIndex, colName, value);
+		checkAndSubmitRow(tr, rowIndex, colName);
 	}
+
+
+	if (isDateField(colName)) {
+		// Evita criar múltiplos inputs se já estiver editando
+		if (!td.find('input').length) {
+			createDateInput(td, value, rowIndex, colName, tr);
+		}
+		return; // Evita salvar o valor antes da validação
+	}
+	// Para outros campos, salva normalmente
+	saveToSession(rowIndex, colName, value);
+	checkAndSubmitRow(tr, rowIndex, colName);
+}
+
+
 
 
 
@@ -86,16 +94,21 @@ function createDateInput(td, value, rowIndex, colName, tr) {
 	td.empty();
 
 	const input = $('<input type="text" maxlength="10" placeholder="dd/mm/aaaa" />')
-		.val(value)
+		.val(value) // adiciona valor ao input
 		.css({
-			'height': '100%',
+			'all': 'unset',
+			'display': 'block',
 			'width': '100%',
-			'box-sizing': 'border-box',
-			'line-height': '1.2',
-			'padding': 0,
-			'margin': 0,
-			'font-size': 'inherit',
-			'border': 'none'
+			'height': '100%',
+			'font': 'inherit',
+			'color': 'inherit',
+			'background': 'none',
+			'border': 'none',
+			'padding': '0',
+			'margin': '0',
+			'outline': 'none',
+			'box-shadow': 'none',
+			'text-align': 'inherit'
 		})
 		.appendTo(td)
 		.mask('00/00/0000')
@@ -122,8 +135,8 @@ function handleDateValidation(input, td, rowIndex, colName, tr) {
 	td.css({ 'border': '1px solid rgb(160, 160, 160)' });
 	const val = input.val().trim();
 
-	input.remove();
-	
+	//input.remove();
+
 	if (val === '' || isValidDate(val)) {
 		td.text(val);
 		input.remove();
@@ -132,9 +145,10 @@ function handleDateValidation(input, td, rowIndex, colName, tr) {
 		saveToSession(rowIndex, colName, val);
 		checkAndSubmitRow(tr, rowIndex);
 	} else {
+		td.text(val);
 		const span = document.querySelector("#invalidDate");
-		span.innerHTML = "Informe uma data válida";
-		td = input.closest('td');
+		span.innerHTML = "Linha com a data válida não será salva";
+		//td = input.closest('td');
 		td.css({ 'border': '2px solid red' });
 	}
 }
@@ -167,25 +181,36 @@ function saveToSession(rowIndex, colName, value) {
 // ---------------------------------------------
 function checkAndSubmitRow(tr, rowIndex, colName) {
 	let rowData = JSON.parse(sessionStorage.getItem('linha_' + rowIndex));
-	console.log('Verificando ID da linha', rowData.id);
 	if (!rowData) return;
 
 	let totalCols = tr.find('td[contenteditable=true]').length;
 	let filledCols = Object.values(rowData).filter(v => v !== '').length;
 
 	// Verifica se todos os campos obrigatórios foram preenchidos
-	if (filledCols !== totalCols) return;
-	console.log('filledCols:', filledCols);
-	console.log('totalCols:', totalCols);
-	
-	// Substitui '/' por '-' nas datas
-	//Mudar posteriormente, para fazer persistencia com uma variavel que não afete a visualização data.
-	if (rowData.dtini) rowData.dtini = rowData.dtini.replace(/\//g, '-');
-	if (rowData.dtfim) rowData.dtfim = rowData.dtfim.replace(/\//g, '-');
+	console.log('totalCols:', totalCols, 'filledCols:', filledCols);
+	// se faltar celulas a preencher retornar para não salvar a linha no banco
+	if (filledCols < totalCols) return;
+	console.log('Verificando ID da linha', rowData.id);
 
-	
-	if (rowData.id == undefined) {
-		sendPost(rowData, tr, rowIndex);
+	//Verifica se a data inicial é maior no que a data final
+	console.log("DTINI: ", rowData.dtini, "DTFIM: ", rowData.dtfim)
+	if (!isValidDateRange(rowData.dtini, rowData.dtfim)) {
+		console.log("", "DTINI: ", rowData.dtini, "DTFIM: ", rowData.dtfim)
+		const span = document.querySelector("#invalidDate");
+		span.innerHTML = "DTINI não pode ser maior que a DTFIM";
+		console.log("DTINI não pode ser maior que a DTFIM")
+		return;
+	}
+	console.log("DATA VALIDA")
+
+	// Substitui '/' por '-' nas datas
+	let objectRequest = rowData;
+	if (objectRequest.dtini) objectRequest.dtini = objectRequest.dtini.replace(/\//g, '-');
+	if (objectRequest.dtfim) objectRequest = objectRequest.dtfim.replace(/\//g, '-');
+
+
+	if (objectRequest.id == undefined) {
+		sendPost(objectRequest, tr, rowIndex);
 	} else {
 		sendPatch(rowData, tr, rowIndex, colName);
 	}
@@ -193,21 +218,24 @@ function checkAndSubmitRow(tr, rowIndex, colName) {
 
 }
 
-function sendPost(rowData, tr, rowIndex) {
+function sendPost(objRequest, tr, rowIndex) {
 	$.ajax({
 		url: '/rowSave',
 		type: 'POST',
 		contentType: 'application/json',
-		data: JSON.stringify(rowData),
+		data: JSON.stringify(objRequest),
 		success: function(response) {
-			//alert('Nova linha salva!');
+			console.log('Nova linha salva!');
 			//tr.attr('data-id', response.id);
 
 			// Se o back-end retornar o ID da nova linha, salve no sessionStorage
 			if (response && response.id) {
 				rowData.id = response.id;
-				sessionStorage.setItem('linha_' + rowIndex, JSON.stringify(rowData));
-				
+
+				response.dtini = response.dtini.replace(/-/g, '/');
+				response.dtfim = response.dtfim.replace(/-/g, '/');
+				sessionStorage.setItem('linha_' + rowIndex, JSON.stringify(response));
+
 			}
 		},
 		error: function() {
@@ -221,26 +249,26 @@ function sendPatch(rowIndex, td, colName) {
 	//pegar o valor atual
 	const rowData = JSON.parse(sessionStorage.getItem('linha_' + rowIndex)) || {};
 	let actualValue = rowData[colName];
-	
+
 	let cellUpdate = {
 		id: rowData.id,
 		campo: td.data('col'),
 		valor: actualValue
 	};
-	
-	console.log(cellUpdate);
+
+	console.log("Celular atualizada ", cellUpdate);
 	$.ajax({
 		url: '/updateCell',
 		type: 'PATCH',
 		contentType: 'application/json',
 		data: JSON.stringify(cellUpdate),
 		success: function() {
-			//alert('Linha atualizada!');
-			
+			console.log('Linha atualizada!');
+
 		},
 		error: function() {
 			//tr.attr('data-status', 'updateError');
-			
+
 		}
 	});
 
@@ -258,4 +286,30 @@ function isValidDate(dateString) {
 		date.getMonth() === month - 1 &&
 		date.getDate() === day
 	);
+}
+
+
+// ---------------------------------------------
+// Função utilitária para validar data inicial é posterior a data final
+// ---------------------------------------------
+function isValidDateRange(dtini, dtfim) {
+	//if (!isValidDate(dtini) || !isValidDate(dtfim)) return false;
+
+	const [d1, m1, y1] = dtini.split('/').map(Number);
+	const [d2, m2, y2] = dtfim.split('/').map(Number);
+
+	const dateIni = new Date(y1, m1 - 1, d1);
+	const dateFim = new Date(y2, m2 - 1, d2);
+	console.log("Passou por DateRange")
+	return dateIni <= dateFim;
+}
+
+function dateFormatForApi(dt) {
+	let dtFormated = dt.replace(/\//g, '-');
+	return dtFormated;
+}
+
+function dateFormatForView(dt) {
+	let dtFormated = dt.replace(/-/g, '/');
+	return dtFormated
 }
